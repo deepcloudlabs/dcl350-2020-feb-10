82e1181563bfda188cf413f073da79f5c173e820
deepcloudlabs <deepcloudlabs@gmail.com>
2020-02-10 20:55:19 +0300

    initial commit

---
+++ hr/src/main/java/com/example/hr/entity/Employee.java
@@ -0,0 +1,132 @@
+package com.example.hr.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "identity", unique = true, nullable = false)
+	private String identityNo;
+	private String fullname;
+	private double salary;
+	@Column(unique = true, nullable = false)
+	private String iban;
+	private int birthYear;
+	@Lob
+	@Column(columnDefinition = "largeblob")
+	private byte[] photo;
+	private boolean fulltime;
+	@Enumerated
+	private Department department;
+
+	public Employee() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getIdentityNo() {
+		return identityNo;
+	}
+
+	public void setIdentityNo(String identityNo) {
+		this.identityNo = identityNo;
+	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public String getIban() {
+		return iban;
+	}
+
+	public void setIban(String iban) {
+		this.iban = iban;
+	}
+
+	public int getBirthYear() {
+		return birthYear;
+	}
+
+	public void setBirthYear(int birthYear) {
+		this.birthYear = birthYear;
+	}
+
+	public byte[] getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(byte[] photo) {
+		this.photo = photo;
+	}
+
+	public boolean isFulltime() {
+		return fulltime;
+	}
+
+	public void setFulltime(boolean fulltime) {
+		this.fulltime = fulltime;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", identityNo=" + identityNo + ", fullname=" + fullname + ", salary=" + salary
+				+ ", iban=" + iban + ", birthYear=" + birthYear + ", fulltime=" + fulltime + ", department="
+				+ department + "]";
+	}
+
+}
