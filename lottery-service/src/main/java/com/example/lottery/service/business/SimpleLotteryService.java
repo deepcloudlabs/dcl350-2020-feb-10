b499e60f769ecb1c162f7bc003b35e666b7e6110
deepcloudlabs <deepcloudlabs@gmail.com>
2020-02-13 11:43:57 +0300

    add actuator to refresh components without restarting service

--- lottery-service/src/main/java/com/example/lottery/service/business/SimpleLotteryService.java
+++ lottery-service/src/main/java/com/example/lottery/service/business/SimpleLotteryService.java
@@ -5,11 +5,18 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 import com.example.lottery.service.LotteryService;
 
+
+// curl localhost:5001/lottery/api/v1/actuator/refresh 
+// -X POST -d {} -H "Content-Type: application/json"
+
+
 @Service
+@RefreshScope
 public class SimpleLotteryService implements LotteryService {
 	@Value("${lottery.min}") private int min;
 	@Value("${lottery.max}") private int max;
